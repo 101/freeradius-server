392d91fa46d17f3fa13fdcf4d1fb42cf88dedac3
101 <alex@linlab.net>
2010-11-17 14:01:51 -0800

    Add init_revocation_store for OCSP verification.

--- src/modules/rlm_eap/types/rlm_eap_tls/rlm_eap_tls.c
+++ src/modules/rlm_eap/types/rlm_eap_tls/rlm_eap_tls.c
@@ -517,6 +517,34 @@ static void eaptls_session_free(UNUSED void *parent, void *data_ptr,
 	pairfree(&vp);
 }
 
+/*
+ *      Create Global X509 revocation store and use it to verify
+ *      OCSP responses
+ *
+ *      - Load the trusted CAs
+ *      - Load the trusted issuer certificates
+ */
+static X509_STORE *init_revocation_store(EAP_TLS_CONF *conf)
+{
+        X509_STORE *store = NULL;
+
+        store = X509_STORE_new();
+
+        /* Load the CAs we trust */
+        if (conf->ca_file || conf->ca_path)
+                if(!X509_STORE_load_locations(store, conf->ca_file, conf->ca_path)) {
+                        radlog(L_ERR, "rlm_eap: X509_STORE error %s", ERR_error_string(ERR_get_error(), NULL));
+                        radlog(L_ERR, "rlm_eap_tls: Error reading Trusted root CA list %s",conf->ca_file );
+                        return NULL;
+                }
+
+#ifdef X509_V_FLAG_CRL_CHECK
+        if (conf->check_crl)
+                X509_STORE_set_flags(store, X509_V_FLAG_CRL_CHECK);
+#endif
+        return store;
+}
+
 
 /*
  *	Create Global context SSL and use it in every new session
